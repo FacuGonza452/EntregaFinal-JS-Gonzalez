c03e23400d5dbcb727a34058526b1d6a5c081974
Facundo Gonzalez <facugonza452@gmail.com>
2023-07-24 12:14:07 -0300

    tercera entrega

--- js/productos.js
+++ js/productos.js
@@ -1,56 +1,129 @@
-// Array de productos
-const productos = [
-  {
-    id: 1,
-    nombre: 'Arroz Oro',
-    precio: 20
-  },
-  {
-    id: 2,
-    nombre: 'Coca Cola 2.25 L',
-    precio: 15
-  },
-  {
-    id: 3,
-    nombre: 'Fideos Molto',
-    precio: 18
-  },
-  {
-    id: 4,
-    nombre: 'Galletas Melba',
-    precio: 10
-  },
-  {
-    id: 5,
-    nombre: 'Galletas Oreo',
-    precio: 14
-  },
-  {
-    id: 6,
-    nombre: 'Salsa de tomate Salsati',
-    precio: 22
-  }
-];
+ // Variables globales
+ let cartItems = {}; // Objeto para almacenar los productos agregados al carrito
+ let isCartExpanded = false; // Variable para mantener el estado del carrito (contraído o expandido)
 
-// Función para añadir productos al carrito
-function addToCart(productId) {
-  const producto = productos.find(p => p.id === productId);
+ // Al cargar la página
+ window.addEventListener("load", () => {
+   const savedCartItems = localStorage.getItem("cartItems");
+   if (savedCartItems) {
+     cartItems = JSON.parse(savedCartItems);
+   }
 
-  if (producto) {
-    console.log(`Producto añadido al carrito: ${producto.nombre}`);
-  }
-}
+   updateCartItemCount();
+   updateCartDropdown();
+ });
 
-// Filtrar productos por rango de precios
-function filtrarProductos(precioMinimo, precioMaximo) {
-  const productosFiltrados = productos.filter(p => p.precio >= precioMinimo && p.precio <= precioMaximo);
-  console.log('Productos filtrados por rango de precios:');
-  console.log(productosFiltrados);
-}
+ function toggleCart() {
+   const cartDropdown = document.getElementById("cartDropdown");
+   isCartExpanded = !isCartExpanded;
+   cartDropdown.style.display = isCartExpanded ? "block" : "none";
+ }
 
-// Buscar productos por nombre
-function buscarProducto(nombre) {
-  const productosEncontrados = productos.filter(p => p.nombre.toLowerCase().includes(nombre.toLowerCase()));
-  console.log('Productos encontrados:');
-  console.log(productosEncontrados);
-}
+ function removeProductFromCart(productId) {
+   if (cartItems[productId]) {
+     cartItems[productId].quantity--;
+
+     if (cartItems[productId].quantity === 0) {
+       delete cartItems[productId];
+     }
+
+     updateCartItemCount();
+     updateCartDropdown();
+     saveCartToLocalStorage();
+   }
+ }
+
+ function addToCart(productId) {
+   const productInfo = getProductInfo(productId);
+
+   if (cartItems[productId]) {
+     cartItems[productId].quantity++;
+   } else {
+     cartItems[productId] = {
+       name: productInfo.name,
+       price: productInfo.price,
+       quantity: 1,
+     };
+   }
+
+   updateCartItemCount();
+   updateCartDropdown();
+   saveCartToLocalStorage();
+ }
+
+ function saveCartToLocalStorage() {
+   localStorage.setItem("cartItems", JSON.stringify(cartItems));
+ }
+
+ function updateCartItemCount() {
+   const itemCountSpan = document.getElementById("cartItemCount");
+   let totalCount = 0;
+
+   for (const productId in cartItems) {
+     totalCount += cartItems[productId].quantity;
+   }
+
+   itemCountSpan.innerText = totalCount;
+ }
+
+ function updateCartDropdown() {
+   const cartDropdown = document.getElementById("cartDropdown");
+   const cartItemsList = document.getElementById("cartItems");
+   cartItemsList.innerHTML = "";
+
+   let totalAmount = 0;
+
+   for (const productId in cartItems) {
+     const product = cartItems[productId];
+
+     const cartItemElem = document.createElement("li");
+     cartItemElem.classList.add("cart-item");
+
+     const itemName = document.createElement("span");
+     itemName.classList.add("item-name");
+     itemName.innerText = `${product.name} (${product.quantity})`;
+
+     const itemPrice = document.createElement("span");
+     itemPrice.classList.add("item-price");
+     itemPrice.innerText = `$${(product.price * product.quantity).toFixed(2)}`;
+
+     const removeButton = document.createElement("button");
+     removeButton.innerText = "X";
+     removeButton.classList.add("remove-button");
+     removeButton.addEventListener("click", () => removeProductFromCart(productId));
+
+     cartItemElem.appendChild(itemName);
+     cartItemElem.appendChild(itemPrice);
+     cartItemElem.appendChild(removeButton);
+
+     cartItemsList.appendChild(cartItemElem);
+
+     totalAmount += product.price * product.quantity;
+   }
+
+   const totalElem = document.createElement("li");
+   totalElem.classList.add("cart-item");
+   totalElem.classList.add("total");
+   totalElem.innerText = `Total a pagar: $${totalAmount.toFixed(2)}`;
+
+   cartItemsList.appendChild(totalElem);
+
+   if (Object.keys(cartItems).length > 0) {
+     cartDropdown.style.display = "block";
+   } else {
+     cartDropdown.style.display = "none";
+   }
+ }
+
+ function getProductInfo(productId) {
+   const products = {
+     1: { name: "Arroz Oro", price: 20 },
+     2: { name: "Coca Cola 2.25 L", price: 15 },
+     3: { name: "Fideos Molto", price: 18 },
+     4: { name: "Galletas Melba", price: 10 },
+     5: { name: "Galletas Oreo", price: 14 },
+     6: { name: "Salsa de tomate Salsati", price: 22 },
+   };
+
+   return products[productId] || { name: "Producto no encontrado", price: 0 };
+ }
